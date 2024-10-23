bc0c5d656900f8636c4aa6405094d447f25aa360
peeydiu <phulong2308@gmail.com>
2024-10-23 13:11:50 +0700

    Initial Commit

---
+++ Approval Matrix/Approval_MatrixApp.swift
@@ -0,0 +1,17 @@
+//
+//  Approval_MatrixApp.swift
+//  Approval Matrix
+//
+//  Created by Trần Long Phú on 23/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Approval_MatrixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Approval MatrixTests/Approval_MatrixTests.swift
@@ -0,0 +1,17 @@
+//
+//  Approval_MatrixTests.swift
+//  Approval MatrixTests
+//
+//  Created by Trần Long Phú on 23/10/24.
+//
+
+import Testing
+@testable import Approval_Matrix
+
+struct Approval_MatrixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
